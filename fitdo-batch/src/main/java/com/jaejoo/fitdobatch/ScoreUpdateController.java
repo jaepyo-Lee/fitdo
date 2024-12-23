04567a4d88ef3b37d77117e27eec57339747007e
jaepyo99 <jaepyo99@naver.com>
2024-12-23 16:30:22 +0900

    fix: 배치 실행 false

---
+++ fitdo-batch/src/main/java/com/jaejoo/fitdobatch/ScoreUpdateController.java
@@ -0,0 +1,17 @@
+package com.jaejoo.fitdobatch;
+
+import com.jaejoo.fitdobatch.launcher.ScoreJobLauncher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ScoreUpdateController {
+    private final ScoreJobLauncher scoreJobLauncher;
+    @GetMapping("/fitdo-batch-lambda/score-batch")
+    public String executeBatch() throws Exception {
+        scoreJobLauncher.launch();
+        return "success";
+    }
+}
