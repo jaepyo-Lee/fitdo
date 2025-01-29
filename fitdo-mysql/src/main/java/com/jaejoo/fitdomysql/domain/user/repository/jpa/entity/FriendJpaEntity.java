682b953b1c4b98c93d5172f5eb9fefa9b8724e84
jaepyo99 <jaepyo99@naver.com>
2025-01-29 23:29:17 +0900

    fix: 친구신청ENUM미사용으로 삭제

--- fitdo-mysql/src/main/java/com/jaejoo/fitdomysql/domain/user/repository/jpa/entity/FriendJpaEntity.java
+++ fitdo-mysql/src/main/java/com/jaejoo/fitdomysql/domain/user/repository/jpa/entity/FriendJpaEntity.java
@@ -21,24 +21,12 @@ public class FriendJpaEntity {
     @JoinColumn(name = "receiver_id")
     private UserJpaEntity receiver;
 
-    @Enumerated(EnumType.STRING)
-    private FriendStatus friendStatus;
-
-    public FriendJpaEntity(UserJpaEntity sender, UserJpaEntity receiver, FriendStatus friendStatus) {
+    public FriendJpaEntity(UserJpaEntity sender, UserJpaEntity receiver) {
         this.sender = sender;
         this.receiver = receiver;
-        this.friendStatus = friendStatus;
     }
 
     public static FriendJpaEntity apply(UserJpaEntity from, UserJpaEntity to) {
-        return new FriendJpaEntity(from, to, FriendStatus.APPLY);
-    }
-
-    public boolean isSupport(FriendStatus status) {
-        return this.friendStatus == status;
-    }
-
-    public void approve() {
-        this.friendStatus = FriendStatus.ACCEPT;
+        return new FriendJpaEntity(from, to);
     }
 }
